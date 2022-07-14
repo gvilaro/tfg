ef06ceec1906a0d5463bc24e246cf6f8f48880d7
gvilaro <59828636+gvilaro@users.noreply.github.com>
2022-07-14 17:46:59 +0200

    presentacio

--- domini/exercit/factoria/FactoriaDePeces.py
+++ domini/exercit/factoria/FactoriaDePeces.py
@@ -1,31 +1,33 @@
-from ..Peca import Peca
-from ..Rei import Rei
-from ..Reina import Reina
-from ..Peo import Peo
-from ..Alfil import Alfil
-from ..Cavall import Cavall
-from ..Torre import Torre
-from...campDeBatalla.Posicio import Posicio
+from __future__ import annotations
 
+import Peca
+import Rei
+import Reina
+import Peo
+import Alfil
+import Cavall
+import Torre
+import Posicio
 
 class FactoriaDePeces:
     @staticmethod
-    def creaPeca(pecaBlanca: bool, fila: int, columna: int) -> Peca:
+    def creaPeca(pecaBlanca: bool, fila: int, columna: int) -> Peca.Peca:
         if fila == 1 or fila == 6:
-            return Peo(pecaBlanca, Posicio(fila, columna))
+            return Peo.Peo(pecaBlanca, Posicio.Posicio(fila, columna))
         elif columna == 0:
-            return Torre(pecaBlanca, Posicio(fila, columna))
+            return Torre.Torre(pecaBlanca, Posicio.Posicio(fila, columna))
         elif columna == 1:
-            return Cavall(pecaBlanca, Posicio(fila, columna))
+            return Cavall.Cavall(pecaBlanca, Posicio.Posicio(fila, columna))
         elif columna == 2:
-            return Alfil(pecaBlanca, Posicio(fila, columna))
+            return Alfil.Alfil(pecaBlanca, Posicio.Posicio(fila, columna))
         elif columna == 3:
-            return Reina(pecaBlanca, Posicio(fila, columna))
+            return Reina.Reina(pecaBlanca, Posicio.Posicio(fila, columna))
         elif columna == 4:
-            return Rei(pecaBlanca, Posicio(fila, columna))
+            return Rei.Rei(pecaBlanca, Posicio.Posicio(fila, columna))
         elif columna == 5:
-            return Alfil(pecaBlanca, Posicio(fila, columna))
+            return Alfil.Alfil(pecaBlanca, Posicio.Posicio(fila, columna))
         elif columna == 6:
-            return Cavall(pecaBlanca, Posicio(fila, columna))
+            return Cavall.Cavall(pecaBlanca, Posicio.Posicio(fila, columna))
         elif columna == 7:
-            return Torre(pecaBlanca, Posicio(fila, columna))
+            return Torre.Torre(pecaBlanca, Posicio.Posicio(fila, columna))
+
